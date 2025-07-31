f7ca01090db74ad0fe26a3ee5f374e7d5135b5de
Abinash Sethi <sethiabinash123@gmail.com>
2025-07-31 01:26:52 -0400

    corrected helm provider

--- 2-efs-install-tf-manifests/4.3-efs-helm-provider.tf
+++ 2-efs-install-tf-manifests/4.3-efs-helm-provider.tf
@@ -4,8 +4,10 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 # HELM Provider
-provider "kubernetes" {
+provider "helm" {
+  kubernetes {
     host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
     cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.cluster.token  
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
 }
